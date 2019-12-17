b8b6079559d313fa43bd47c17781fc895cb562bd
Arnim Bleier <arnim.bleier@gmail.com>
2019-12-17 23:59:57 +0100

    add read.SentiWS

---
+++ SentiWS_v1.8c/SentiWS.R
@@ -0,0 +1,35 @@
+library(data.table)
+
+
+# The read.SentiWS function read the SentiWS files and a data.table.
+# Code adapted from PolMine/sentiws.R (by Andreas Blätte)
+# https://gist.github.com/PolMine/70eeb095328070c18bd00ee087272adf
+
+read.SentiWS <- function(){
+
+    .unfold <- function(.SD){
+    pos <- gsub("^([A-Z]+)\\s+.*$", "\\1", .SD[["data"]][1])
+    weight <- as.numeric(gsub("^[A-Z]+\\s+(-?\\d\\.\\d+).*$", "\\1", .SD[["data"]][1]))
+    words <- gsub("^[A-Z]+\\s+-?\\d\\.\\d+\\s*(.*?)\\s*$", "\\1", .SD[["data"]][1])
+    words <- if (!grepl("^\\s*$", words)) strsplit(x = words, split = ",")[[1]] else NULL
+    list(
+      word = c(.SD[["word"]][1], words),
+      base = c(TRUE, rep(FALSE, times = length(words))),
+      lemma = .SD[["word"]][1],
+      pos = pos,
+      weight = weight
+    )
+  }
+
+    dts <- lapply(
+    c(positive = "SentiWS_v1.8c_Positive.txt", negative = "SentiWS_v1.8c_Negative.txt"),
+    function(filename){
+      dt <- fread(filename)
+      colnames(dt) <- c("word", "data")
+      dt[, "id" :=  1L:nrow(dt)]
+      dt[, .unfold(.SD), by = c("id")]
+    }
+  )
+
+    as.data.table(rbindlist(dts)[,c("word","lemma","weight")])
+}
